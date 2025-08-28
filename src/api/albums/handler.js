d8873408d5b3ae53148da20ba6d651e60022c3ea
Irfan Dwi Samudra <irfandwisamudra08@gmail.com>
2025-08-29 05:00:59 +0700

    feat(handler): use auto-bind to ensure proper context in AlbumsHandler and SongsHandler

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -1,7 +1,11 @@
+const autoBind = require("auto-bind");
+
 class AlbumsHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
+
+    autoBind(this);
   }
 
   async postAlbumHandler(request, h) {
--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -1,7 +1,11 @@
+const autoBind = require("auto-bind");
+
 class SongsHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
+
+    autoBind(this);
   }
 
   async postSongHandler(request, h) {
